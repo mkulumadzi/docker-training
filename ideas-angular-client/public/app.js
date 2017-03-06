597fcc012759fa92b8769dd8183549edae1a7b43
Evan Waters <evan.waters@gmail.com>
2017-03-05 20:09:53 -0700

    Cleaned up client app

--- ideas-angular-client/public/app.js
+++ ideas-angular-client/public/app.js
@@ -3,27 +3,30 @@ var ideasApp = angular.module('ideasApp', []);
 function ideasController($scope, $http) {
   $scope.formData = {};
 
-  // Get existing ideas and display them.
-  $http.get('/api/ideas')
-    .success(function(data) {
-      $scope.ideas = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-
   // Create a new idea
   $scope.createIdea = function() {
     $http.post('/api/ideas', $scope.formData)
     .success(function(data) {
       $scope.formData = {}; // Clear the form data
-      $scope.totos = data;
-      console.log(data);
+      $scope.loadData();
     })
     .error(function(data) {
       console.log('Error: ' + data);
     });
   };
+
+  // Load data
+  $scope.loadData = function() {
+    $http.get('/api/ideas')
+      .success(function(data) {
+        $scope.ideas = data;
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  // Initial load
+  $scope.loadData();
 
 }
